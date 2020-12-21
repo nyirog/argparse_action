20bf18ed2b0d0aa6446341747b5ce3ae3a31d363
Nyiro Gergo <gergo.nyiro@gmail.com>
2020-12-21 09:20:49 +0100

    setup.py: refraze short description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fp:
 setup(
     name="argparse_action",
     version="0.1.1",
-    description="untility module for argparse to create cli ",
+    description="confgiure argparse from function signature",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="Apache 2",
