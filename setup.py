4a63170cfdec61c61fe2611e55c3706de5a2139c
Nyiro Gergo <gergo@nyiro.name>
2021-12-20 11:46:58 +0100

    version: 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="argparse_action",
-    version="0.3.0",
+    version="0.3.1",
     description="confgiure argparse from function signature",
     long_description=long_description,
     long_description_content_type="text/markdown",
