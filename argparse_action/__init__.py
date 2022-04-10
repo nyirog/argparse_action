f5711b6001852f43ba25b6b38b45965a1c137bed
Gergo Nyiro <gergo@nyiro.name>
2022-04-10 21:54:51 +0200

    argparse_action.logging.init_logging: default log handler
    The default log handler was the `StreamHandler`. The user can define the
    default log handler from now on. If no default log handler is defined
    then `init_logging` will act as before and use the `StreamHandler` as
    default log handler.
    
    `SysLogHandler` can be created with the `create_syslog_handler` helper
    function which select "user" facility and "/dev/log" address by default.

--- argparse_action/__init__.py
+++ argparse_action/__init__.py
@@ -1,2 +1,2 @@
 from .action import Action, add_action
-from .logging import add_log_arguments, init_logging
+from .logging import add_log_arguments, init_logging, create_syslog_handler
--- argparse_action/logging.py
+++ argparse_action/logging.py
@@ -42,14 +42,17 @@ def add_log_arguments(parser):
         "--log-none", action="store_true", help="Disable logging"
     )
     destination_group.add_argument("--log-file", help="Log into LOG_FILE")
+    destination_group.add_argument(
+        "--log-console", action="store_true", help="Log into console"
+    )
 
 
-def init_logging(namespace):
+def init_logging(namespace, default_handler=None):
     log_level = _LOG_LEVEL[namespace.log_level]
 
     formatter = logging.Formatter(namespace.log_format, datefmt=namespace.log_datefmt)
 
-    handler = _create_handler(namespace)
+    handler = _create_handler(namespace) or default_handler or logging.StreamHandler()
     handler.setLevel(log_level)
     handler.setFormatter(formatter)
 
@@ -66,7 +69,14 @@ def _create_handler(namespace):
         return logging.FileHandler(namespace.log_file)
 
     if namespace.log_syslog:
-        facility = logging.handlers.SysLogHandler.facility_names[namespace.log_syslog]
-        return logging.handlers.SysLogHandler(facility=facility, address="/dev/null")
+        return create_syslog_handler(namespace.log_syslog)
 
-    return logging.StreamHandler()
+    if namespace.log_console:
+        return logging.StreamHandler()
+
+    return None
+
+
+def create_syslog_handler(facility="user", address="/dev/log"):
+    facility = logging.handlers.SysLogHandler.facility_names[facility]
+    return logging.handlers.SysLogHandler(facility=facility, address=address)
